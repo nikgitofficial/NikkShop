e2504572412ad4ef60a2e3b614d0e1ba86d29388
Nikko Paceño <npaceo@sitea.local>
2026-03-23 20:26:13 +0800

    fixing the middleware auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,15 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+    cookieName:
+      process.env.NODE_ENV === "production"
+        ? "__Secure-next-auth.session-token"
+        : "next-auth.session-token",
+  });
+
   const { pathname } = req.nextUrl;
   const role = (token as any)?.role;
 
